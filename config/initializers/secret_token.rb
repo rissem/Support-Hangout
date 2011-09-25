8a0f356316213a1840374fff9994ced22a8d1fb2
Mike Risse <rissem@gmail.com>
2011-09-24 17:01:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SupportHangout::Application.config.secret_token = 'cba15648c203671bb947374133b5a6ada5dc36349dd360f76ec4a3c599a741c3312926bebe6f12191ecd084e1596573ac43ccfe8c27051227df6b0bc6b664ca8'
