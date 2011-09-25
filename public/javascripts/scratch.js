ac269c180b20cb47c188a597dda2f7a91c123d9c
Erik Larson <erikl@alum.mit.edu>
2011-09-25 11:29:50 -0700

    Added base html file

--- public/javascripts/scratch.js
+++ public/javascripts/scratch.js
@@ -5,5 +5,19 @@ function doStuff() {
 gapi.hangout.addApiReadyListener(startMyApp);
 
 function startMyApp(){
-	console.log("This worked");
+	var participant_id=gapi.hangout.getParticipantId();
+	var participants=gapi.hangout.getParticipants();
+	console.log("Participants:",participants);
+	
+	gapi.hangout.addParticipantAddedListener(newUserAdded);
+}
+
+function newUserAdded(new_participants){
+	for (i=0;i<new_participants.length;i++){
+		gapi.hangout.displayNotice(new_participants[i].displayName + " arrived);
+	}
+	gapi.hangout.displayNotice(
+	  ,
+	  opt_permanent
+	)
 }
